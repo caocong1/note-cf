3ee4cb0c142bdda936a00fe1f909f943420dd079
sorawatcher <caocong1@gmail.com>
2024-03-31 15:20:14 +0800

    feat:api to home

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,8 @@ let videoStream: MediaStream | null = null
 const files: Record<string, File> = {}
 let streamingId: string
 const chunkSize = 1024 * 1024 * 2
-// const apiUrl = 'http://home.love2c.cc:2096'
-const apiUrl = 'https://home.note.pet'
+const apiUrl = 'http://home.love2c.cc:2096'
+// const apiUrl = 'https://home.note.pet:2096'
 const socket = io(apiUrl);
 
 function PeerCallStream(peers: string[] = []) {
