f53b3657ee69f1e7c267e1aa98db1ca5983bb357
sorawatcher <caocong1@gmail.com>
2024-04-03 00:26:02 +0800

    chore: 修改打包分片

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,42 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        // manualChunks: {
+        //   react: ['react', 'react-is', 'react/jsx-runtime'],
+        //   'react-dom': ['react-dom'],
+        // },
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if(id.includes('/react-dom')){
+              return 'react-dom'
+            }
+            if(id.includes('/react')){
+              return 'react'
+            }
+            if(id.includes('rc-')){
+              return 'rc'
+            }
+            if(id.includes('@ant-design+icons')){
+              return 'icons'
+            }
+            if(id.includes('@ant-design+')){
+              return 'antd1'
+            }
+            if(id.includes('antd')){
+              return 'antd'
+            }
+            if(id.includes('babel')){
+              return 'babel'
+            }
+            console.log(id)
+          }
+          
+        }
+      },
+    },
+  },
   plugins: [react()],
 })
